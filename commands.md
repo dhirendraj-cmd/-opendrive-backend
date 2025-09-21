d6ec6976ebda5edece627b85a737f9ddc172ff50
dhirendraj-cmd <djha5051@gmail.com>
2025-09-21 21:15:15 +0530

    file upload endpoint created

--- commands.md
+++ commands.md
@@ -3,4 +3,5 @@ pip install sqlmodel
 pip install "python-jose[cryptography]"
 pip install bcrypt==3.2.2
 pip install passlib
+pip install python-multipart
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
---
+++ opendrive/uploaders/upload_routes.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, APIRouter, UploadFile, File, HTTPException
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+import os
+import shutil
+
+upload_router = APIRouter()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+@upload_router.post("/uploadfiles/")
+async def create_upload_file(files: Annotated[list[UploadFile], File()]):
+    save_files = []
+    if not os.path.exists(os.path.join(BASE_DIR, "uploads")):
+        os.makedirs(os.path.join(BASE_DIR, "uploads"), exist_ok=True)
+
+    for file in files:
+        file_path = os.path.join(BASE_DIR, "uploads")
+        save_path = f"{file_path}/{file.filename}"
+        with open(save_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        save_files.append({"filename": file.filename})
+
+    return save_files
+
+
+@upload_router.get("/", response_class=HTMLResponse)
+async def show_file_form():
+    content = """
+                <body>
+                <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+                <input name="files" type="file" multiple>
+                <input type="submit">
+                </form>
+                </body>
+            """ 
+    
+    return HTMLResponse(content=content)
