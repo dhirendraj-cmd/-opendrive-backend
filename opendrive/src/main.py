4f74d0a68d8e132f4fbca1ceb542973ca6252339
dhirendraj-cmd <djha5051@gmail.com>
2025-09-21 23:17:32 +0530

    cors allowed for upload endpoint

--- opendrive/src/main.py
+++ opendrive/src/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from opendrive.uploaders.upload_routes import upload_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
 app.include_router(upload_router)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],     # Allows all standard HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],
+)
 
 
 
