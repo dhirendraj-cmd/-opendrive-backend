207bef6c2b8503d81315b2e28c52e98f096be69c
dhirendraj-cmd <djha5051@gmail.com>
2025-11-14 14:44:14 +0530

    refactored refresh token logic

--- opendrive/uploaders/upload_routes.py
+++ opendrive/uploaders/upload_routes.py
@@ -17,19 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 
 @upload_router.post("/uploadfiles/")
-async def create_upload_file(uploaded_file = Depends(upload_file_loggedin_user)):
+def create_upload_file(uploaded_file = Depends(upload_file_loggedin_user)):
     return uploaded_file
-    
-# async def create_upload_file(files: Annotated[list[UploadFile], File()]):
-    # save_files = []
-    # if not os.path.exists(os.path.join(BASE_DIR, "uploads")):
-    #     os.makedirs(os.path.join(BASE_DIR, "uploads"), exist_ok=True)
 
-    # for file in files:
-    #     file_path = os.path.join(BASE_DIR, "uploads")
-    #     save_path = f"{file_path}/{file.filename}"
-    #     with open(save_path, "wb") as buffer:
-    #         shutil.copyfileobj(file.file, buffer)
-    #     save_files.append({"filename": file.filename})
 
-    # return save_files
+
+
+
