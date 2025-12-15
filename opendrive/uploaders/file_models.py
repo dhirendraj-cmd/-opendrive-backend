d69e7f8295177df64570be6e53ec151e50690d61
dhirendraj-cmd <djha5051@gmail.com>
2025-12-16 00:40:14 +0530

    adding folder model

--- opendrive/uploaders/file_models.py
+++ opendrive/uploaders/file_models.py
@@ -5,9 +5,9 @@ from .upload_schemas import FileDataSchema
 
 
 # builtins imports
-from typing import Optional, TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING, List
 from datetime import datetime
-from sqlmodel import Field, Relationship
+from sqlmodel import Field, Relationship, SQLModel
 
 
 
@@ -16,14 +16,31 @@ if TYPE_CHECKING:
 
 
 
+class Folder(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
+    folder_key: str = Field(unique=True, index=True)
+    display_name: str = Field(default="Pi Drive")
+    user_id: int = Field(foreign_key="user.id", nullable=False, ondelete="CASCADE")
+    created_at: datetime = Field(default=now_utc)
+    updated_at: datetime = Field(default=now_utc)
+
+    # back populates
+    user: Optional["User"] = Relationship(back_populates="folders")
+    files: List["FileDataToBeStored"] = Relationship(back_populates="folder")
+    
+    
+
+
 
 class FileDataToBeStored(FileDataSchema, table=True):
     id: int = Field(primary_key=True, default=None)
     user_id: int = Field(foreign_key="user.id", nullable=False, ondelete="CASCADE")
+    folder_id: int = Field(foreign_key="folder.id", nullable=False, ondelete="CASCADE")
     created_at: datetime = Field(default_factory=now_utc)
     updated_at: datetime = Field(default_factory=now_utc)
 
-    # back populate to User model
+    # back populate
     user: Optional["User"] = Relationship(back_populates="files")
+    folder: Optional["Folder"] = Relationship(back_populates="files")
 
 
